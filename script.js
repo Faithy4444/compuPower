5d54735b34bcb35dc66c984627dfb2fee4e8bc8e
Faithy4444 <161722786+Faithy4444@users.noreply.github.com>
2026-08-18 21:26:14 +0200

    cart item fixes

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let cart = [];
 
 const addToCart = (product, price) => {
     let cartItems = document.getElementById("cart-items");
-    cart.push(product, price);
-    cartItems.innerHTML = cart
+    cart.push({ product, price });
+    cartItems.innerHTML = JSON.stringify(cart);
+
+    return (cart)
 
 }
-
-console.log(addToCart("car", "2000"))
