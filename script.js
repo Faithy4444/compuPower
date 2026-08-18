926eadfa4aa04c993a48e65787044bd8f2e0aa0d
Faithy4444 <161722786+Faithy4444@users.noreply.github.com>
2026-08-18 21:02:07 +0200

    add to cart fuctionality

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+let cart = [];
+
+const addToCart = (product, price) => {
+    let cartItems = document.getElementById("cart-items");
+    cart.push(product, price);
+    cartItems.innerHTML = cart
+
+}
+
+console.log(addToCart("car", "2000"))
