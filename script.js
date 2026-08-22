26ea8c7836ebacd6569cc113380c5aa7b7734e43
Faithy4444 <161722786+Faithy4444@users.noreply.github.com>
2026-08-22 10:35:06 +0200

    refactored code

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-
 let cart = [];
 
 const addToCart = (product, price) => {
-    let cartItems = document.getElementById("cart-items");
-    cart.push({ product, price });
-    let output = "";
 
-    cart.forEach(item => {
-        output += `
-            <div class="cart-item">
-                <h3>${item.product}</h3>
-                <p>R${item.price}</p>
-            </div>
-        `;
+    cart.push({
+        product: product,
+        price: price,
+        quantity: 1
     });
 
-    cartItems.innerHTML = output;
-
-    return cart;
-
-}
+    console.log(cart);
+};
